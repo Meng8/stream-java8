14a0d7f9a8be68a6ce700ee69aab2b784b627641
Meng8 <1098167361@qq.com>
2017-11-26 22:22:40 +0800

    完成一个上台阶的问题，一个人每次可以上1、2或者3级太极，问10级台阶有多少种上法。在stream-java8的基础上修改和提交

--- src/main/java/FibonacciSupplier.java
+++ src/main/java/FibonacciSupplier.java
@@ -4,15 +4,24 @@ import java.util.function.Supplier;
  * Created by qiuzhanghua on 2017/2/7.
  */
 public class FibonacciSupplier implements Supplier<Long> {
-  private long previous = 1;
-  private long current = 2;
+//  private long previous = 1;
+//  private long current = 2;
+	private long f = 0;
+	private long n = 0;
+	private long s= 1;
+	
 
   @Override
   public Long get() {
-    long p = previous;
-    long next = current + previous;
-    previous = current;
-    current = next;
-    return p;
+//    long p = previous;
+//    long next = current + previous;
+//    previous = current;
+//    current = next;
+//    return p;
+    long sum=f+n+s;
+	f=n;
+	n=s;
+	s=sum;
+	return sum;
   }
 }
